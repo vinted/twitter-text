12665e6dba28d038d7afec5ca828d3e8c0c2d8d4
Algimantas Backys <algimantas.backys@vinted.com>
2024-01-17 16:41:10 +0200

    add package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+// Copyright 2018 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+import PackageDescription
+
+let package = Package(
+    name: "TwitterTextName",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "TwitterTextLib", targets: ["TwitterTextTarget"])
+    ],
+    targets: [
+        .target(
+            name: "TwitterTextTarget",
+            path: "objc/lib"
+        )
+    ]
+)
